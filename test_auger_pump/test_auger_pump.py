ce3967c178bf6828e5ce4687c5f872a948bdb210
Yuemin (Helen) Mao <yueminm@andrew.cmu.edu>
2023-04-03 12:51:00 -0400

    Update test_auger_pump.py

--- test_auger_pump/test_auger_pump.py
+++ test_auger_pump/test_auger_pump.py
@@ -12,14 +12,19 @@ board = Arduino('COM4')
 
 A_RPWM = board.get_pin('d:3:p')
 A_LPWM = board.get_pin('d:5:p')
-P_RPWM = board.get_pin('d:9:p')
-P_LPWM = board.get_pin('d:10:p')
+A_RENABLE = board.get_pin('d:4:o')
+A_LENABLE = board.get_pin('d:2:o')
+# P_RPWM = board.get_pin('d:9:p')
+# P_LPWM = board.get_pin('d:10:p')
 
 while True:  
     A_RPWM.write(0) 
     A_LPWM.write(0)
-    P_RPWM.write(0)
-    P_LPWM.write(0)
+    A_RENABLE.write(1)
+    A_LENABLE.write(1)
+    
+    # P_RPWM.write(0)
+    # P_LPWM.write(0)
     
     if keyboard.is_pressed('w'):
         print('auger forward')
@@ -29,12 +34,12 @@ while True:
     
     if keyboard.is_pressed('s'):
         print('auger backward')
-        P_LPWM.write(0)
-        P_RPWM.write(0.5)
+        A_LPWM.write(0)
+        A_RPWM.write(0.5)
         time.sleep(0.1)
         
-    if keyboard.is_pressed('p'):
-        print('pump on')
-        P_LPWM.write(0.5)
-        P_RPWM.write(0)
-        time.sleep(0.1)
+    # if keyboard.is_pressed('p'):
+    #     print('pump on')
+    #     P_LPWM.write(0.5)
+    #     P_RPWM.write(0)
+    #     time.sleep(0.1)
