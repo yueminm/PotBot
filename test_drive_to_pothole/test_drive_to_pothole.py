b832cef9350e41388f3d07397ade20aec8005910
Yuemin (Helen) Mao <yueminm@andrew.cmu.edu>
2023-04-05 10:50:26 -0400

    integrated vision and driving

--- test_drive_to_pothole/test_drive_to_pothole.py
+++ test_drive_to_pothole/test_drive_to_pothole.py
@@ -1,6 +1,11 @@
 from pyfirmata import Arduino, util
 import time
 import keyboard
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import math
+from pothole_detection_hist_video import getThresh, imageInfo
 
 # Monkey patch to fix python 3.11 bug
 import inspect
@@ -16,50 +21,37 @@ L_LPWM = board.get_pin('d:5:p')
 R_RPWM = board.get_pin('d:9:p')
 R_LPWM = board.get_pin('d:10:p')
 
+vid = cv2.VideoCapture(0)
+
 while True:
     L_LPWM.write(0)
     L_RPWM.write(0)
     R_LPWM.write(0)
     R_RPWM.write(0)
     
-    if keyboard.is_pressed('w'):
+    _, frame = vid.read()
+    thresh = getThresh(frame)
+    _, _, xValue, yValue = imageInfo(frame,thresh)
+    if yValue <= 680 and yValue >= 600:
         print('forward')
         L_LPWM.write(0.5)
         L_RPWM.write(0)
         R_LPWM.write(0.5)
         R_RPWM.write(0)
         time.sleep(0.1)
-
-    if keyboard.is_pressed('s'):
-        print('backward')
-        L_LPWM.write(0)
-        L_RPWM.write(0.5)
-        R_LPWM.write(0)
-        R_RPWM.write(0.5)
-        time.sleep(0.1)
-
-    if keyboard.is_pressed('a'):
+    
+    if yValue < 600:
         print('left')
         L_LPWM.write(0)
         L_RPWM.write(0.5)
         R_LPWM.write(0.5)
         R_RPWM.write(0)
         time.sleep(0.1)
-
-    if keyboard.is_pressed('d'):
+    
+    if yValue > 680:
         print('right')
         L_LPWM.write(0.5)
         L_RPWM.write(0)
         R_LPWM.write(0)
         R_RPWM.write(0.5)
         time.sleep(0.1)
-
-    if keyboard.is_pressed('q'):
-        print('stop')
-        L_LPWM.write(0)
-        L_RPWM.write(0)
-        R_LPWM.write(0)
-        R_RPWM.write(0)
-        time.sleep(0.1)
-
-
