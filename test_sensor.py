ccc509822f483091677b953d9c6227a9d44b4fec
Yuemin (Helen) Mao <yueminm@andrew.cmu.edu>
2023-04-20 17:20:00 -0400

    Update test_sensor.py

--- test_sensor.py
+++ test_sensor.py
@@ -23,31 +23,42 @@ start = 0
 end = 0
 
 while True:
-    trig.write(0)
-    # time.sleep(0.5)
-    board.pass_time(0.5)
-    trig.write(1)   
-    # time.sleep(0.00001)
-    board.pass_time(0.00001)
-    trig.write(0)
+    # trig.write(0)
+    # # time.sleep(0.5)
+    # board.pass_time(0.5)
+    # trig.write(1)   
+    # # time.sleep(0.00001)
+    # board.pass_time(0.00001)
+    # trig.write(0)
     
-    # print(echo.read())
-    while echo.read() == False:
-        pass
-    start = time.time()
-        # break
-
+    # # print(echo.read())
+    # while echo.read() == False:
+    #     pass
     # start = time.time()
+    #     # break
 
-    while echo.read() == True:
-        pass
-    end = time.time()
-        # break
-        # print(2)
+    # # start = time.time()
+
+    # while echo.read() == True:
+    #     pass
+    # end = time.time()
+    #     # break
+    #     # print(2)
 
     
-    TimeElapsed = end - start
-    # print("start:  ", start, "  end:  ", "  Elapse: ",TimeElapsed)
-    distance = (TimeElapsed * 343) / 2
+    # TimeElapsed = end - start
+    # # print("start:  ", start, "  end:  ", "  Elapse: ",TimeElapsed)
+    # distance = (TimeElapsed * 343) / 2
 
-    print("Measured Distance = {} cm".format(distance))
+    # print("Measured Distance = {} cm".format(distance))
+    
+    trig.write(True)
+    time.sleep(0.00001)
+    trig.write(False)
+
+    # Read the distance
+    duration = echo.read_duration()
+    distance = duration / 58.2
+
+    print('Distance:', distance, 'cm')
+    time.sleep(1)
