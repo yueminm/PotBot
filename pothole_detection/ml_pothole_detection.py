629fc3b680c3408b127aff1978871bf13a706cc7
Ivy He <hexiao24381@gmail.com>
2023-04-21 02:14:16 -0400

    Add YOLOv5. See README for setup instruction.

---
+++ pothole_detection/ml_pothole_detection.py
@@ -0,0 +1,17 @@
+## open camera source and load the yolov5 pothole detection model
+
+# import all libraries
+
+import argparse
+import os
+import platform
+import sys
+from pathlib import Path
+
+import torch
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
